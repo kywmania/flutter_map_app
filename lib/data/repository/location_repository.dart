0ea9642389baeab9cd5788b2e370b58a1d7b4d47
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-04-13 17:32:52 +0900

    Feat: Location_repository class

---
+++ lib/data/repository/location_repository.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_map_app/data/model/location_model.dart';
+import 'package:http/http.dart';
+
+class LocationRepository {
+  Future<List<LocationModel>?> search(String query) async {
+    await dotenv.load();
+
+    try {
+      Client client = Client();
+      Response response = await client.get(
+          Uri.parse(
+              'https://openapi.naver.com/v1/search/blog.json?query=$query'),
+          headers: {
+            'X-Naver-Client-Id': dotenv.env['NAVER_CLIENT_ID'] ?? '',
+            'X-Naver-Client-Secret': dotenv.env['NAVER_CLIENT_SECRET'] ?? '',
+          });
+
+      if (response.statusCode == 200) {
+        final map = jsonDecode(response.body);
+        return List.from(map["items"])
+            .map((e) => LocationModel.fromJson(e))
+            .toList();
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+}
